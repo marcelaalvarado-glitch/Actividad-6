08d38b22ebe8d6d7d09bf378aca44e0a5f9002a7
4º PC <4 PC@DESKTOP-6342417>
2026-03-02 16:34:06 +0100

    Agregar archivos de proyecto.

---
+++ Actividad 6/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Actividad_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Saludo";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hola" + textBox1.Text;
+            textBox1.Text = "";
+        }
+    }
+}
